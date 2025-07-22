eb98c4faa7231813c0776d8c180618fc5f179808
RAHUL <rahulguptaa1211@gmail.com>
2025-07-22 12:11:43 +0530

    "navbar logo updated "

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       <Image
         className="cursor-pointer w-28 md:w-32"
         onClick={() => router.push('/')}
-        src={assets.logo}
+        src={assets.logo1 }
         alt="logo"
       />
       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
